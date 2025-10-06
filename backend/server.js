c611e093b9e1c47faca46108964f32be367b844b
Kuldeep Gade <gadekuldeep25@gmail.com>
2025-10-06 19:37:35 +0530

    backend landing page created

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
@@ -6,14 +7,17 @@ import path from "path";
 import { connectDB } from "./config/db.js";
 import authRoutes from "./routes/authRoutes.js";
 import vaultRoutes from "./routes/vaultRoutes.js";
+import landingRoute from "./routes/landingRoute.js";
 
 dotenv.config();
 connectDB();
+
 
 const app = express();
 app.use(cors());
 app.use(bodyParser.json({ limit: "2mb" }));
 
+app.use("/api/",landingRoute);
 app.use("/api/auth", authRoutes);
 app.use("/api/vault", vaultRoutes);
 
